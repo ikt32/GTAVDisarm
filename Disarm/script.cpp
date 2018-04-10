92d6646e0c649fb6c387189758abdcb72ab376cc
E66666666 <emily66666666ok@outlook.com>
2018-04-10 22:38:10 +0200

    Clear damage state; Fix weapon damage detection

--- Disarm/script.cpp
+++ Disarm/script.cpp
@@ -20,15 +20,26 @@ void update()
 	Ped peds[2048];
 	int nPeds = worldGetAllPeds(peds, 2048);
 	for (int i = 0; i < nPeds; i++) {
-		int bone;
-		PED::GET_PED_LAST_DAMAGE_BONE(peds[i], &bone);
-		Weapon allPedWeapon = WEAPON::GET_CURRENT_PED_WEAPON_ENTITY_INDEX(peds[i]);
-		if (allPedWeapon) {
-			if (ENTITY::HAS_ENTITY_BEEN_DAMAGED_BY_ANY_OBJECT(allPedWeapon) ||
-				bone == BONE_RIGHT_HAND || bone == BONE_RIGHT_FOREARM) {
+        if (peds[i] == playerPed) 
+            continue;
+		Weapon weapon = WEAPON::GET_CURRENT_PED_WEAPON_ENTITY_INDEX(peds[i]);
+		if (weapon) {
+            int bone;
+            PED::GET_PED_LAST_DAMAGE_BONE(peds[i], &bone);
+		    if (bone == BONE_RIGHT_HAND || bone == BONE_RIGHT_FOREARM) {
 				WEAPON::SET_PED_DROPS_WEAPON(peds[i]);
 				PED::CLEAR_PED_LAST_DAMAGE_BONE(peds[i]);
 			}
+            else {
+                PED::CLEAR_PED_LAST_DAMAGE_BONE(peds[i]);
+            }
+            if (ENTITY::HAS_ENTITY_BEEN_DAMAGED_BY_ANY_PED(weapon)) {
+                WEAPON::SET_PED_DROPS_WEAPON(peds[i]);
+                ENTITY::CLEAR_ENTITY_LAST_DAMAGE_ENTITY(weapon);
+            }
+            else {
+                ENTITY::CLEAR_ENTITY_LAST_DAMAGE_ENTITY(weapon);
+            }
 		}
 	}
 }
